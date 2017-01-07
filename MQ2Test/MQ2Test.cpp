8cde08e3679de2eaaab244afc7bcfba39cae84ff
brainiac <brainiac2k@gmail.com>
2017-01-07 02:46:10 -0800

    Add new feature to allow extending existing data types
    New functions:
    - AddMQ2TypeExtension(const char* szName, MQ2Type* extension)
    - RemoveMQ2TypeExtension(const char* szName, MQ2Type* extension)
    
    These functions allow you to extend an existing type by providing the
    name of the type you want to extend and the custom MQ2Type instance that
    does the extending.
    
    Currently this works only for members. It will check for members of the
    extension type before checking members of the base type. Its sort of
    like inheritance, but doesn't require modifying the original type.
    
    See MQ2Test/MQ2Test.cpp for an example.
    
    Also made some improvements and optimizations to the MQ2DataAPI code.

---
+++ MQ2Test/MQ2Test.cpp
@@ -0,0 +1,86 @@
+// MQ2Test.cpp : Defines the entry point for the DLL application.
+//
+
+// PLUGIN_API is only to be used for callbacks.  All existing callbacks at this time
+// are shown below. Remove the ones your plugin does not use.  Always use Initialize
+// and Shutdown for setup and cleanup, do NOT do it in DllMain.
+
+
+#include "../MQ2Plugin.h"
+
+PreSetup("MQ2Test");
+
+//----------------------------------------------------------------------------
+// test the mq2 datatype extension code
+
+class MQ2CharacterExtensionType* pCharExtType = nullptr;
+
+class MQ2CharacterExtensionType : public MQ2Type
+{
+public:
+	enum ExtensionMembers {
+		Test1 = 1,
+		Test2 = 2
+	};
+
+	MQ2CharacterExtensionType() : MQ2Type("MQ2TestCharacterExtension")
+	{
+		TypeMember(Test1);
+		TypeMember(Test2);
+	}
+
+	bool GETMEMBER()
+	{
+		PMQ2TYPEMEMBER pMember = FindMember(Member);
+		if (!pMember)
+			return false;
+		switch (pMember->ID) {
+		case Test1:
+			Dest.DWord = 1;
+			Dest.Type = pIntType;
+			return true;
+		case Test2:
+			Dest.DWord = 2;
+			Dest.Type = pIntType;
+			return true;
+		}
+
+		return false;
+	}
+
+	bool ToString(MQ2VARPTR VarPtr, PCHAR Destination)
+	{
+		return false;
+	}
+
+	bool FromData(MQ2VARPTR& VarPtr, MQ2TYPEVAR& Source)
+	{
+		if (Source.Type != pCharExtType)
+			return false;
+		VarPtr.Ptr = Source.Ptr;
+		return true;
+	}
+
+	bool FromString(MQ2VARPTR& VarPtr, PCHAR Source)
+	{
+		return false;
+	}
+};
+
+// Called once, when the plugin is to initialize
+PLUGIN_API VOID InitializePlugin(VOID)
+{
+	DebugSpewAlways("Initializing MQ2Test");
+
+	pCharExtType = new MQ2CharacterExtensionType;
+	AddMQ2TypeExtension("character", pCharExtType);
+}
+
+// Called once, when the plugin is to shutdown
+PLUGIN_API VOID ShutdownPlugin(VOID)
+{
+	DebugSpewAlways("Shutting down MQ2Test");
+
+	RemoveMQ2TypeExtension("character", pCharExtType);
+	delete pCharExtType;
+}
